534e9784a49a4e149590bb508341866ad0178a5c
oluwadara,abijo@gmail.com <oluwadara.abijo@gmail.com>
2020-12-02 10:55:14 +0100

    feat: Allow conversion both ways

--- app/src/main/java/com/dara/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/dara/currencyconverter/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dara.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
@@ -14,15 +16,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var amount: String
     private lateinit var targetCurrency: String
     private lateinit var targetCurrencyListener: AdapterView.OnItemSelectedListener
-
+    private lateinit var baseAmountWatcher: TextWatcher
+    private lateinit var targetAmountWatcher: TextWatcher
+    private var isReversed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        spinner_from.isEnabled = false
-        rates = arrayListOf()
-        getRates()
 
         targetCurrencyListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -32,12 +32,18 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 targetCurrency = rates[position].currency
+                tv_currency_to.text = targetCurrency
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
         }
 
+        spinner_from.isEnabled = false
+        rates = arrayListOf()
+
+        getRates()
+        observeAmountInput()
         btn_convert.setOnClickListener { convert() }
 
     }
@@ -71,13 +77,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun convert() {
-        amount = edit_amt_from.text.toString()
         if (amount.isNotEmpty()) {
             val rate = rates.first { it.currency == targetCurrency }.rate
-            val result = amount.toFloat() * rate
-            edit_amt_to.setText(result.toString())
+            if (isReversed) {
+                val result = amount.toFloat() / rate
+                edit_amt_from.setText(result.toString())
+            } else {
+                val result = amount.toFloat() * rate
+                edit_amt_to.setText(result.toString())
+            }
         } else {
             Toast.makeText(this, "Enter an amount", Toast.LENGTH_SHORT).show()
         }
     }
+
+    /**
+     * Observes amount inputs and allows conversion both ways, depending on the amount input
+     * field that was changed last
+     */
+    private fun observeAmountInput() {
+        // Initialize text watchers to observe amount inputs
+        baseAmountWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                isReversed = false
+                amount = edit_amt_from.text.toString()
+            }
+        }
+        edit_amt_from.addTextChangedListener(baseAmountWatcher)
+
+        targetAmountWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                isReversed = true
+                amount = edit_amt_to.text.toString()
+
+            }
+        }
+        edit_amt_to.addTextChangedListener(targetAmountWatcher)
+    }
+
 }
